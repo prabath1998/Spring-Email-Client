4a7337c07c60077b590bd03df380d9456c49be12
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-05 08:45:10 +0530

    send simple mail

--- src/main/java/com/test/emailclient/SpringEmailClientApplication.java
+++ src/main/java/com/test/emailclient/SpringEmailClientApplication.java
@@ -1,13 +1,27 @@
 package com.test.emailclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringEmailClientApplication {
+	@Autowired
+	private EmailSenderService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmailClientApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail(){
+		service.sendSimpleEmail(
+				"prabhathudayanga1998@gmail.com",
+				"This is email body",
+				"This is email subject"
+		);
 	}
 
 }
